b870accdc3b372abf84d84c862f0124a07f6bde1
Allan Jiang <origami789@gmail.com>
2020-05-20 00:50:42 -0700

    update to tf2.0

--- initial-model/train.py
+++ initial-model/train.py
@@ -2,12 +2,14 @@ from utils import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_threads', default=100)
-parser.add_argument('--n_samples', default=1000)
+parser.add_argument('--num_threads', type=int, default=50)
+parser.add_argument('--n_samples', type=int, default=1000)
 parser.add_argument('--annotations_path', default='/Users/jiangts/Documents/stanford/cs231n/final_project/semantic_annotations')
 parser.add_argument('--images_path', default='/Users/jiangts/Documents/stanford/cs231n/final_project/combined')
 args = parser.parse_args()
 
 
-train_ds, test_ds, validation_ds = parse_into_data_sets(args.annotations_path, args.n_samples, args.num_threads)
+train_ds, test_ds, validation_ds = parse_into_data_sets(args.annotations_path,
+        args.n_samples,
+        args.num_threads)
 
--- initial-model/utils.py
+++ initial-model/utils.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import glob
-import random
 import os
 import itertools
 from multiprocessing import Pool
@@ -8,7 +6,8 @@ import json
 import time
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
+tf.random.set_seed(0)
 
 
 def _download_and_clean_file(filename, url):
@@ -34,31 +33,38 @@ def _download_and_clean_file(filename, url):
                 file_object.write(line)
     tf.io.gfile.remove(temp_file)
 
-def decode_img(img):
-	img = tf.image.decode_jpeg(img, channels=3)
-	img = tf.image.convert_image_dtype(img, tf.float32)
-	return img
 
 def process_path(file_name):
 	img = tf.io.read_file(file_name)
-	img = decode_img(img)
+	img = tf.image.decode_image(img, channels=3)
 	return img
 
 
 def load_label_pair(file_name):
 	img = process_path(file_name)
 	root, _ = os.path.split(file_name)
+        # generalize: subtract extension from end of path
 	num = file_name.split('/')[-1].split('.png')[0]
 	data_path = os.path.join(root, num + '.json')
 	with open(data_path, 'r') as f:
 		labeled_data = json.load(f)
+		# print('fname', file_name)
                 # TODO check multiple children
 		for component in labeled_data['children']:
-			x_min, x_max = component['bounds'][0], component['bounds'][0] + component['bounds'][2]
-			y_min, y_max = component['bounds'][1], component['bounds'][1] + component['bounds'][3]
-			img_crop = img[x_min:x_max, y_min:y_max]
+			x_min, y_min, x_max, y_max = component['bounds']
+			# print('crop dims', [y_min,y_max], [x_min,x_max])
+			# print('img dims', img.shape)
+			try:
+				img_crop = img[y_min:y_max, x_min:x_max]
+			except Exception as e:
+				print(e)
+				traceback(print_exc())
+				print()
+				raise e
+			# print('cropped', img_crop)
                         # currently only fetching label
 			label = component['componentLabel']
+			# print('label', label)
 			return img_crop, label
 	print(data_path)
 
@@ -66,15 +72,20 @@ def parse_into_data_sets(local_dir, num_files, num_threads):
 	b = time.time()
 	images = tf.io.gfile.glob(os.path.join(local_dir, '*.png'))[:num_files]
 	print('Number of images', len(images))
-	random.shuffle(images)
-	p = Pool(processes=num_threads)
-	ret = p.map(load_label_pair, images)
+	print("got glob", time.time() - b)
+
+	b = time.time()
+	tf.random.shuffle(images)
+	# p = Pool(processes=num_threads)
+	# ret = p.map(load_label_pair, images)
+	ret = [load_label_pair(image) for image in images]
 	print("finish load", time.time() - b)
 
 	b = time.time()
 	valid_pairs = [x for x in ret if x is not None]
 	labels = set([x[1] for x in valid_pairs])
 	label_mapping = {}
+        # TODO make fixed mapping
 	for idx, key in enumerate(labels):
 		label_mapping[key] = [idx]
 	data_points = [(x[0], label_mapping[x[1]]) for x in valid_pairs]
@@ -82,16 +93,15 @@ def parse_into_data_sets(local_dir, num_files, num_threads):
 
 	b = time.time()
 	# split 60/20/20
-	# train_pts = data_points[0:int(0.6*(len(data_points)))]
-	train_pts = data_points
+	train_pts = data_points[0:int(0.6*(len(data_points)))]
 	test_pts = data_points[int(0.6 * len(data_points)):int(0.8*len(data_points))]
 	validation_pts = data_points[int(0.8*len(data_points)):]
 	print("split dataset", time.time() - b)
 
 	b = time.time()
-	train_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in train_pts), (tf.float32, tf.int32))
-	test_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
-	validation_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
+	train_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in train_pts), (tf.uint8, tf.uint8))
+	test_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.uint8, tf.uint8))
+	validation_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.uint8, tf.uint8))
 	# sample one point to make sure the ds is loaded properly
 	for image, label in train_ds.take(1):
 		print("Image shape: ", image.numpy().shape)
