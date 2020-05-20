0de9740e1eb5fb001b247e4e0ff2c87c84c72c23
Allan Jiang <origami789@gmail.com>
2020-05-19 23:40:44 -0700

    load data

--- initial-model/train.py
+++ initial-model/train.py
@@ -1,12 +1,13 @@
-from utils import * 
+from utils import *
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_threads', default=100)
 parser.add_argument('--n_samples', default=1000)
+parser.add_argument('--annotations_path', default='/Users/jiangts/Documents/stanford/cs231n/final_project/semantic_annotations')
+parser.add_argument('--images_path', default='/Users/jiangts/Documents/stanford/cs231n/final_project/combined')
 args = parser.parse_args()
 
-local_dir = '/Users/maxchang/Downloads/semantic_annotations'
 
-train_ds, test_ds, validation_ds = parse_into_data_sets(local_dir, args.n_samples, args.num_threads)
+train_ds, test_ds, validation_ds = parse_into_data_sets(args.annotations_path, args.n_samples, args.num_threads)
 
--- initial-model/utils.py
+++ initial-model/utils.py
@@ -5,6 +5,7 @@ import os
 import itertools
 from multiprocessing import Pool
 import json
+import time
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 tf.enable_eager_execution()
@@ -33,58 +34,69 @@ def _download_and_clean_file(filename, url):
                 file_object.write(line)
     tf.io.gfile.remove(temp_file)
 
-def decode_img(img): 
+def decode_img(img):
 	img = tf.image.decode_jpeg(img, channels=3)
 	img = tf.image.convert_image_dtype(img, tf.float32)
-	return img 
+	return img
 
-def process_path(file_name): 
+def process_path(file_name):
 	img = tf.io.read_file(file_name)
 	img = decode_img(img)
 	return img
-	
 
-def load_label_pair(file_name): 
+
+def load_label_pair(file_name):
 	img = process_path(file_name)
 	root, _ = os.path.split(file_name)
 	num = file_name.split('/')[-1].split('.png')[0]
 	data_path = os.path.join(root, num + '.json')
 	with open(data_path, 'r') as f:
 		labeled_data = json.load(f)
+                # TODO check multiple children
 		for component in labeled_data['children']:
 			x_min, x_max = component['bounds'][0], component['bounds'][0] + component['bounds'][2]
 			y_min, y_max = component['bounds'][1], component['bounds'][1] + component['bounds'][3]
 			img_crop = img[x_min:x_max, y_min:y_max]
+                        # currently only fetching label
 			label = component['componentLabel']
 			return img_crop, label
 	print(data_path)
 
 def parse_into_data_sets(local_dir, num_files, num_threads):
-	images = glob.glob(os.path.join(local_dir, '*.png'))[:num_files]
+	b = time.time()
+	images = tf.io.gfile.glob(os.path.join(local_dir, '*.png'))[:num_files]
 	print('Number of images', len(images))
-	random.shuffle(images) 
+	random.shuffle(images)
 	p = Pool(processes=num_threads)
 	ret = p.map(load_label_pair, images)
+	print("finish load", time.time() - b)
+
+	b = time.time()
 	valid_pairs = [x for x in ret if x is not None]
 	labels = set([x[1] for x in valid_pairs])
 	label_mapping = {}
-	for idx, key in enumerate(labels): 
-		label_mapping[key] = [idx] 
+	for idx, key in enumerate(labels):
+		label_mapping[key] = [idx]
 	data_points = [(x[0], label_mapping[x[1]]) for x in valid_pairs]
-	
-	# split 20/20/20 
-	train_pts = data_points[0:int(0.6*(len(data_points)))]
+	print("got labels", time.time() - b)
+
+	b = time.time()
+	# split 60/20/20
+	# train_pts = data_points[0:int(0.6*(len(data_points)))]
+	train_pts = data_points
 	test_pts = data_points[int(0.6 * len(data_points)):int(0.8*len(data_points))]
 	validation_pts = data_points[int(0.8*len(data_points)):]
+	print("split dataset", time.time() - b)
 
+	b = time.time()
 	train_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in train_pts), (tf.float32, tf.int32))
-	# sample one point to make sure the ds is loaded properly 
+	test_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
+	validation_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
+	# sample one point to make sure the ds is loaded properly
 	for image, label in train_ds.take(1):
 		print("Image shape: ", image.numpy().shape)
 		print("Label: ", label.numpy())
-	test_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
-	validation_ds = tf.data.Dataset.from_generator(lambda: (pair for pair in test_pts), (tf.float32, tf.int32))
-	
-	return train_ds, test_ds, validation_ds	
+	print("from dataset generator", time.time() - b)
 
-	
+	return train_ds, test_ds, validation_ds
+
